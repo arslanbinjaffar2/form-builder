063ae10906d8e29cc7730887720ff51cb008709e
ahmad13054 <mah@eventbuizz.com>
2023-06-14 11:06:50 +0500

    RS20-795

--- src/app/contexts/CreateQuestionContext.jsx
+++ src/app/contexts/CreateQuestionContext.jsx
@@ -619,6 +619,8 @@ class CreateQuestionContextProvider extends Component {
 
           if(_section[_sectionIndex].questions === undefined || _section[_sectionIndex].questions.length <= 0){
             _section[_sectionIndex].questions = [_clone];
+            _section[_sectionIndex].questions[0].form_builder_form_id = parseInt(form_id);
+            _section[_sectionIndex].questions[0].form_builder_section_id = parseInt(_section[_sectionIndex].id);
             _section[_sectionIndex].questions[0].active = true;
           }else{
             _questionIndex = _section[_sectionIndex].questions.findIndex(x => x.active === true) !== -1 ? _section[_sectionIndex].questions.findIndex(x => x.active === true) : 0;
@@ -626,6 +628,7 @@ class CreateQuestionContextProvider extends Component {
             _section[_sectionIndex].questions[_questionIndex].active = false;
             _section[_sectionIndex].questions[_questionIndex + 1].active = true;
             _section[_sectionIndex].questions[_questionIndex + 1].form_builder_form_id = parseInt(form_id);
+            _section[_sectionIndex].questions[_questionIndex + 1].form_builder_section_id = parseInt(_section[_sectionIndex].id);
             _section[_sectionIndex].questions.forEach((element, k) => {
               element.sort_order = k;
             });          
@@ -1337,7 +1340,7 @@ class CreateQuestionContextProvider extends Component {
         console.log(response)
         if(response.data.status === 1){
           this.setState({
-            loading:false,
+            updating:false,
             data:{...response.data.data, sections:response.data.data.sections.map((item, i)=>{
                 if(i === 0 && item.questions !== undefined && item.questions.length > 0){
                     return   {...item, active: i === 0 ? true : false, questions: item.questions.map((q, i)=>( {...q, active: i === 0 ? true : false} ))};
@@ -1348,8 +1351,8 @@ class CreateQuestionContextProvider extends Component {
       }
       else{
         this.setState({
-          loading:false,
-          loadingError:response.data.message
+          updating:false,
+          updatingError:null,
         })
       }
       } catch (error) {
